9771f50ec92807336b6a326429232969ae466394
Yury <yury.connect@gmail.com>
2024-09-15 21:54:00 +0300

    solution: task1602

--- 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1602/src/task1602/Solution.java
+++ 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1602/src/task1602/Solution.java
@@ -15,9 +15,21 @@ Requirements:
 5. Метод run класса TestThread должен выводить "it's a run method".
 6. Метод main не изменять.*/
 
+
 public class Solution {
     public static void main(String[] args) {
         TestThread thread = new TestThread();
         thread.start();
+    }
+
+    public static class TestThread extends Thread {
+        static {
+            System.out.println("it's a static block inside TestThread");
+        }
+
+        @Override
+        public void run() {
+            System.out.println("it's a run method");
+        }
     }
 }
