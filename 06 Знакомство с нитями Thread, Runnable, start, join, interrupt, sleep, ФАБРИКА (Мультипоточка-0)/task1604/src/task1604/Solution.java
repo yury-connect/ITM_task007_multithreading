b6be00f8df3a96a58f1eef94b9e5a6e670038173
Yury <yury.connect@gmail.com>
2024-09-17 16:26:20 +0300

    solution: task1604

--- 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1604/src/task1604/Solution.java
+++ 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1604/src/task1604/Solution.java
@@ -14,15 +14,31 @@ Requirements:
 3. Класс SpecialThread должен реализовывать интерфейс Runnable.
 4. Метод run класса SpecialThread должен выводить свой стек-трейс.*/
 
+import java.util.stream.IntStream;
+
+
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new SpecialThread());
+        Thread thread = new Thread(new SpecialThread()); // Создаем новый поток (Thread) с задачей SpecialThread (Runnable). Это означает, что поток будет выполнять метод run() из SpecialThread при запуске.
         thread.start();
 
         System.out.println("*****************");
 
-        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
-            System.out.println(element);
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) { // Вывод стек-трейса для текущего потока (главного потока)
+            System.out.println("поток_1 (Главный): " + element);
+        }
+    }
+
+
+    public static class SpecialThread implements Runnable {
+
+        @Override
+        public void run() {
+            StackTraceElement[] stackTraceElements = new RuntimeException().getStackTrace();
+            IntStream.range(0, stackTraceElements.length)
+                    .forEach(count -> System.out.printf(
+                            "поток_2 // Элемент СтэкТрэйса под № '%d' , имеет значение: '%s'\n"
+                            , count, stackTraceElements[count]));
         }
     }
 }
