5bd2a445c73b899d9d533a1dac0393eb0d7655f4
Yury_VikaNoteBookLinuxFedora <yury.connect@gmail.com>
2024-09-23 19:51:22 +0300

    Solution -> GPT-help: task1707

--- 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1707(Singleton)/src/task1707/IMF.java
+++ 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1707(Singleton)/src/task1707/IMF.java
@@ -1,15 +1,24 @@
 package task1707;
 
-
 public class IMF {
 
     private static IMF imf;
 
+    // Публичный статический метод для получения единственного экземпляра IMF
     public static IMF getFund() {
-        //add your code here - добавь код тут
+        // Синхронизируемся на классе IMF для потокобезопасности
+        if (imf == null) {
+            synchronized (IMF.class) {
+                if (imf == null) {
+                    imf = new IMF();
+                }
+            }
+        }
         return imf;
     }
 
+    // Приватный конструктор, чтобы предотвратить создание объектов извне
     private IMF() {
     }
 }
+
