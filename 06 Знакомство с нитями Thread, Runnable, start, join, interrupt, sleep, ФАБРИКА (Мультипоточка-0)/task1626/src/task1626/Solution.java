904019803b50d245bd3e67861af8cb81987df5a3
Yury_VikaNoteBookLinuxFedora <yury.connect@gmail.com>
2024-09-23 16:24:26 +0300

    Solution -> GPT-help: task1626

--- 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1626/src/task1626/Solution.java
+++ 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1626/src/task1626/Solution.java
@@ -21,10 +21,26 @@ public class Solution {
         new Thread(new CountUpRunnable(), "Увеличиваем").start();
     }
 
-    public static class CountUpRunnable {
-        //Add your code here - добавь код тут
-    }
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp = 1;  // Начальное значение для отсчета
 
+        @Override
+        public void run() {
+            try {
+                while (countIndexUp <= Solution.number) {
+                    System.out.println(toString());
+                    countIndexUp += 1;
+                    Thread.sleep(500);  // Задержка на 500 миллисекунд
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        @Override
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
+    }
 
     public static class CountdownRunnable implements Runnable {
         private int countIndexDown = Solution.number;
@@ -46,3 +62,22 @@ public class Solution {
         }
     }
 }
+
+
+
+
+
+/*
+Объяснение:
+Класс CountUpRunnable:
+
+Реализует интерфейс Runnable.
+В методе run мы используем цикл, который увеличивает счетчик с 1 до 5,
+выводя его значение каждые 500 миллисекунд с помощью Thread.sleep(500).
+Счетчик увеличивается до тех пор, пока он не достигнет 5 (значение переменной Solution.number).
+Выводится имя потока и текущее значение счетчика через метод toString.
+Реализация в методе main:
+
+В методе main создаются и запускаются два потока:
+один для убывающего счета (CountdownRunnable), другой для увеличивающегося счета (CountUpRunnable).
+ */
