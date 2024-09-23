06d882d82942836e656eddaef8c60ba044c949f5
Yury_VikaNoteBookLinuxFedora <yury.connect@gmail.com>
2024-09-23 19:48:38 +0300

    Solution -> GPT-help: task1706

--- 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1706(Singleton)/src/task1706/OurPresident.java
+++ 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1706(Singleton)/src/task1706/OurPresident.java
@@ -3,10 +3,21 @@ package task1706;
 public class OurPresident {
     private static OurPresident president;
 
+    // Приватный конструктор для предотвращения создания объектов извне
     private OurPresident() {
     }
 
+    // Статический метод для получения единственного экземпляра
     public static OurPresident getOurPresident() {
         return president;
+    }
+
+    // Статический блок с синхронизацией для инициализации president
+    static {
+        synchronized (OurPresident.class) {
+            if (president == null) {
+                president = new OurPresident();
+            }
+        }
     }
 }
