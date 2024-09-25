0c5146989c3c9c35231f47f823564d739de58f3c
Yury_Home_PC <yury.connect@gmail.com>
2024-09-25 22:33:58 +0300

    solution: task1706

--- 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1706(Singleton)/src/task1706/OurPresident.java
+++ 07 Знакомство с нитями synchronized, volatile, yield, CRUD, Comparble, Транзакционность (Мультипоточка-1)/task1706(Singleton)/src/task1706/OurPresident.java
@@ -4,6 +4,15 @@ public class OurPresident {
     private static OurPresident president;
 
     private OurPresident() {
+        // Приватный конструктор для предотвращения создания объектов извне
+    }
+
+    static { // Статический блок с синхронизацией для инициализации president
+        synchronized (OurPresident.class) {
+            if (president == null) {
+                president = new OurPresident();
+            }
+        }
     }
 
     public static OurPresident getOurPresident() {
